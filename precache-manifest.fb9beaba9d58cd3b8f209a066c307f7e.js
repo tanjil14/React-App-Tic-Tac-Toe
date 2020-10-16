f0c7f7960fb00e65e43e7e8f132fc30ac419dbc3
tanjil <tanjil14@cse.pstu.ac.bd>
2020-10-16 10:09:38 +0600

    Updates

---
+++ precache-manifest.fb9beaba9d58cd3b8f209a066c307f7e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd04342c9161b5deb05bc996ad0f3bba",
+    "url": "/React-App-Tic-Tac-Toe/index.html"
+  },
+  {
+    "revision": "3c55dc1508ac7da5609d",
+    "url": "/React-App-Tic-Tac-Toe/static/css/main.30dfcd09.chunk.css"
+  },
+  {
+    "revision": "06d8ab26e8d9f676b8d6",
+    "url": "/React-App-Tic-Tac-Toe/static/js/2.ea017610.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-App-Tic-Tac-Toe/static/js/2.ea017610.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c55dc1508ac7da5609d",
+    "url": "/React-App-Tic-Tac-Toe/static/js/main.bb89bb8d.chunk.js"
+  },
+  {
+    "revision": "ecef2dab679b9d299db3",
+    "url": "/React-App-Tic-Tac-Toe/static/js/runtime-main.6b2557cc.js"
+  }
+]);
